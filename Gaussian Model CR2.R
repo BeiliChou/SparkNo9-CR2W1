4d6702052732eb93749565c0726cb4cf62088a5e
Beili Chou <55406270+BeiliChou@users.noreply.github.com>
2020-12-03 14:37:59 -0500

    Post Meeting 12/3

--- Gaussian Model CR2.R
+++ Gaussian Model CR2.R
@@ -1,6 +1,14 @@
-CR2W1_Filtered <- CR2W1_Main[!is.na(CR2W1_Main$`Lead Rate`),] %>% filter(`Lead Rate` != 0)
+CR2W1_Main <- CR2W1_Main %>% rename(Lead_Rate = `Lead Rate`)
+CR2W1_Filtered <- CR2W1_Main[!is.na(CR2W1_Main$Lead_Rate),] %>% filter(Lead_Rate != 0)
+CR2W1_Filtered <- CR2W1_Filtered %>% mutate(Emotional = 1)
 CR2W1_Filtered$Leads_Per_100_Impressions <- CR2W1_Filtered$Leads / CR2W1_Filtered$Impressions * 100
-preds <- CR2W1_Filtered %>% glm(Leads_Per_100_Impressions ~ Brand_Position + Audience, family = "gaussian",.)
+CR2W1_Filtered[CR2W1_Filtered$Creative == "Made Clear", "Emotional"] <- 0
+CR2W1_Filtered[CR2W1_Filtered$Creative == "Are You In?", "Emotional"] <- 0
+CR2W1_Filtered[CR2W1_Filtered$Creative == "Stop The Sale", "Emotional"] <- 0
+CR2W1_Filtered[CR2W1_Filtered$Creative == "Data Footprint", "Emotional"] <- 0
+preds <- CR2W1_Filtered %>% glm(Leads_Per_100_Impressions ~ Brand_Position + Audience + Emotional, family = "gaussian",.)
 CR2W1_Filtered$preds <- predict(preds)
 View(CR2W1_Filtered)
 write.csv(CR2W1_Filtered, file = "CR2W1 Filtered.csv")     
+
+CR2W1_Filtered %>% group_by(Creative) %>% summarise(avg_pred_ldper100 = mean(preds)) %>% arrange(desc(avg_pred_ldper100))
--- Initial Findings.R
+++ Initial Findings.R
@@ -1,6 +1,6 @@
 library(readxl);library(dplyr)
 CR2W1_Main <- read_excel("CR2W1 Main.xlsx")
-CR2W1_Main <- CR2W1_Main %>% mutate(Brand_Position = `Brand Position`)
+CR2W1_Main <- CR2W1_Main %>% rename(Brand_Position = `Brand Position`)
 CR2W1_Main <- CR2W1_Main[-33,]
 CR2W1_Analysis <- CR2W1_Main %>% group_by(Brand_Position, Audience) %>% filter(Clicks>20) %>% summarise(Total_Spent = sum(Spent), Impressions_Total = sum(Impressions), 
                                                                 Leads_Total = sum(Leads), Leads_Per_100_Impressions = (Leads_Total/Impressions_Total)*100,
