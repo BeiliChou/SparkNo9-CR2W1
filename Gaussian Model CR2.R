83b5b5f8ce2d7ab87f2afa4407b21df8ff0893b3
Beili Chou <55406270+BeiliChou@users.noreply.github.com>
2020-12-03 09:27:11 -0500

    Add files via upload

--- Gaussian Model CR2.R
+++ Gaussian Model CR2.R
@@ -3,3 +3,4 @@ CR2W1_Filtered$Leads_Per_100_Impressions <- CR2W1_Filtered$Leads / CR2W1_Filtere
 preds <- CR2W1_Filtered %>% glm(Leads_Per_100_Impressions ~ Brand_Position + Audience, family = "gaussian",.)
 CR2W1_Filtered$preds <- predict(preds)
 View(CR2W1_Filtered)
+write.csv(CR2W1_Filtered, file = "CR2W1 Filtered.csv")     
